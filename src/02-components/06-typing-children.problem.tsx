d2311c8c704c47fbdc4779734ccef877bb332d58
YJ You <yyou017@gmail.com>
2023-06-10 14:55:11 +1200

    Solve Q7

--- src/02-components/06-typing-children.problem.tsx
+++ src/02-components/06-typing-children.problem.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const Button = (props: { children: React.ReactNode }) => {
+export const Button = (props: {}) => {
   return <button>{props.children}</button>;
 };
 
@@ -13,3 +13,14 @@ const Parent = () => {
     </>
   );
 };
+
+<div
+  className={classNames(
+    "absolute z-[99] flex h-[calc(100vh-4rem)] transform select-none flex-col border-2 border-secondary-701 bg-secondary-800 py-1 pt-5 text-white transition-all duration-300",
+    !sidebarIsOpen ? "w-min px-6" : " w-max px-10",
+    props.className
+  )}
+  onMouseOver={() => setSidebarIsOpen(true)}
+  onMouseLeave={() => handleMouseLeave()}
+  // style={{ position: pinSideBar ? "relative" : "fixed" }}
+></div>;
