5b4b0aa3dddea1619657e76b71a92e4a9f50401d
YJ You <yyou017@gmail.com>
2024-09-21 14:19:29 +1200

    Solve 08-64.5

--- src/08-advanced-patterns/64.5-record-of-components-with-same-props.problem.tsx
+++ src/08-advanced-patterns/64.5-record-of-components-with-same-props.problem.tsx
@@ -22,9 +22,9 @@ const COMPONENTS = {
   password: (props) => {
     return <input {...props} type="password" />;
   },
-};
+} satisfies Record<string, React.ComponentType<InputProps>>
 
-export const Input = (props: unknown) => {
+export const Input = (props: {type: keyof typeof COMPONENTS} & InputProps) => {
   const Component = COMPONENTS[props.type];
   return <Component {...props} />;
 };
