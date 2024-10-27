686328a551931a93386aa9482ae6d9a61dda4591
YJ You <yyou017@gmail.com>
2024-10-27 18:28:00 +1300

    Solve 02-08

--- src/02-components/08-using-html-props.problem.tsx
+++ src/02-components/08-using-html-props.problem.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-export const Button = ({ className, ...rest }: {}) => {
+export const Button = ({ className, ...rest }: {
+  className?: string
+  onClick: React.MouseEventHandler
+  type: "button" | "submit" | "reset"
+}) => {
   return (
     <button {...rest} className={`default-classname ${className}`}></button>
   );
