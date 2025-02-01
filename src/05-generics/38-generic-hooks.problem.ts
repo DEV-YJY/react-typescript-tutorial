4b4f87c3bfc0cb636fd4d4e3e807b55d473e3c3c
YJ You <yyou017@gmail.com>
2025-02-01 13:48:06 +1300

    Solve 05-39

--- src/05-generics/38-generic-hooks.problem.ts
+++ src/05-generics/38-generic-hooks.problem.ts
@@ -8,7 +8,8 @@ import { Equal, Expect } from "../helpers/type-utils";
  *
  * There are _many_ different solutions - but they all involve generics.
  */
-export const useStateAsObject = (initial: any) => {
+
+export const useStateAsObject= (initial: any)  => {
   const [value, set] = useState(initial);
 
   return {
